386bd231ae1d9d692b1d0c7a0c7a11515faa736f
pete.j97 <ecjung97@gmail.com>
2026-04-30 00:00:22 +0900

    Initial Commit

---
+++ FastPhotoReviewer/FastPhotoReviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastPhotoReviewerApp.swift
+//  FastPhotoReviewer
+//
+//  Created by 정은총 on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct FastPhotoReviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
